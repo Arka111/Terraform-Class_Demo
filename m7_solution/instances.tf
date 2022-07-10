fd4771c94e52b71cc45097b1242b8d10d2c3c298
Arkajyoti Misra <arkajyoti.misra@gmail.com>
2022-07-10 14:04:14 +0530

    Update instances.tf

--- m7_solution/instances.tf
+++ m7_solution/instances.tf
@@ -18,6 +18,7 @@ resource "aws_instance" "nginx" {
   subnet_id              = aws_subnet.subnets[(count.index % var.vpc_subnet_count)].id
   vpc_security_group_ids = [aws_security_group.nginx-sg.id]
   iam_instance_profile   = aws_iam_instance_profile.nginx_profile.name
+  key_name               = "tf_learn.pem"
   depends_on             = [aws_iam_role_policy.allow_s3_all]
 
   user_data = templatefile("${path.module}/startup_script.tpl", {
